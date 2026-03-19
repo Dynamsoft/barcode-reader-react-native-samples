05c43007ec6b64221d6ffad986ed6623195cb88b
DMGithubPublisher <github@dynamsoft.com>
2026-03-19 13:34:11 +0800

    update to v11.4.1010

--- ScanDriversLicense/src/DriverLicenseScanner.tsx
+++ ScanDriversLicense/src/DriverLicenseScanner.tsx
@@ -81,6 +81,8 @@ export function ScannerView({onComplete}: ScannerViewProps) {
       cvr.stopCapturing();
       camera.close();
       camera.setCameraView(null);
+      cvr.dispose();
+      camera.dispose();
     };
   }, [camera, cameraView, cvr, onComplete]);
 
